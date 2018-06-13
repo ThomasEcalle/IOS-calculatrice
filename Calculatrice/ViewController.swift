c9b1b49198703bc125ab57d149cd9997f9bf58b6
etudiant <etudiant@iMac-de-etudiant-14.local>
2018-06-13 12:47:24 +0200

    Init

---
+++ Calculatrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculatrice
+//
+//  Created by etudiant on 04/05/2018.
+//  Copyright © 2018 thomasecalle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
